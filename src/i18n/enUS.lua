608a796ef5fd1f2bb3fb8d0d3a01d2384fb65cd9
ramblin-rise <FranklinSimmons@praleen.io>
2025-10-25 12:26:42 -0400

    Using literal for addon name

--- src/i18n/enUS.lua
+++ src/i18n/enUS.lua
@@ -1,6 +1,6 @@
 local addonName = select(1, ...)
 local L = LibStub("AceLocale-3.0"):NewLocale(addonName, "enUS", true)
-L.addOnName = addonName
+L.addOnName = "Flight Master"
 L.addOnSlashCmd = "fm"
 L.on = "on"
 L.off = "off"
